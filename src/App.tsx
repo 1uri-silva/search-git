6728f9ca1c11c4df600337df8033be4769d4f57d
iuri <iurirodrigues2200@gmail.com>
2021-09-07 08:39:57 -0300

    api axios

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Home from './pages/home';
+import Global from './global/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Home />
+      <Global />
+    </>
+  );
+};
+
+export default App;
